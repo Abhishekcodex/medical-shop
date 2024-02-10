e72c2aa775e28ab4929b32a31deb1b8738a40b64
Abhishekcodex <104084107+Abhishekcodex@users.noreply.github.com>
2024-02-10 17:50:12 +0530

    add
    add

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const config = {
+    db: {
+      host: "localhost",
+      user: "root",
+      password: "G(t)d0nk2KTDe5gC",
+      database: "emedical",
+    },    
+  };
+  module.exports = config;
